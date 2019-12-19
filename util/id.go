17d987bce0512079ed89e41cbdc8095178cc3a2e
kelub <852996883@qq.com>
2019-12-19 20:59:34 +0800

    add  get unique inc ID

---
+++ util/id.go
@@ -0,0 +1,20 @@
+package util
+
+import "sync"
+
+var preSec int64 = 0
+var mu sync.Mutex
+
+var gIdx int64 = 0
+
+func getIdx(now int64) int64{
+	mu.Lock()
+	defer mu.Unlock()
+	if preSec != now{
+		preSec = now
+		gIdx = 0
+	}
+	gIdx++
+	ret  := gIdx
+	return ret
+}
